7140d892fc98ea0fe57a97802a916ad0a38d7404
Josh Wiley <joshwiley115@gmail.com>
2016-04-04 23:18:30 -0400

    Brokenified

--- broken.mk
+++ broken.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/samsung/lt03wifi/full_lt03wifi.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
+# Inherit some common broken stuff.
+$(call inherit-product, vendor/broken/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := cm_lt03wifi
+PRODUCT_NAME := broken_lt03wifi
 PRODUCT_DEVICE := lt03wifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
