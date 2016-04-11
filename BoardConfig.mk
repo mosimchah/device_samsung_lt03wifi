395c78d100b96e6329156deaff392d83519b64d1
Orion116 <Joshwiley115@gmail.com>
2016-04-10 21:11:39 -0400

    Disable dexopt

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,7 +151,7 @@ BOARD_SYSTEMIMAGE_JOURNAL_SIZE := 0
 TARGET_POWERHAL_VARIANT := samsung
 
 # Enable dex-preoptimization to speed up first boot sequence
-WITH_DEXPREOPT := true
+# WITH_DEXPREOPT := true
 
 # UMS
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/class/android_usb/android0/f_mass_storage/lun%d/file
