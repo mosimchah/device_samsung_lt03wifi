f298d19670b38b9ccd5145c9035ce499ad859c8e
Josh Wiley <joshwiley115@gmail.com>
2016-06-21 23:39:41 -0400

    Added device maintainer

--- broken.mk
+++ broken.mk
@@ -11,3 +11,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=lt03wifi \
     PRODUCT_DEVICE=lt03wifi \
     TARGET_DEVICE=lt03wifi
+
+# Broken Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+	DEVICE_MAINTAINERS="Josh Wiley (Orion116)"
