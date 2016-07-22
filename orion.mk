3a7d0978785f0ac098fb8f2f72afa8f5b56b2476
mosimchah <mosimchah@gmail.com>
2016-07-22 18:20:29 -0400

    now it will get past the "lunch menu"

--- orion.mk
+++ orion.mk
@@ -1,16 +1,13 @@
 # Inherit some common orion stuff.
 $(call inherit-product, vendor/orion/config/common_tablet_wifionly.mk)
 
-PRODUCT_NAME := orion_lt03wifi
-PRODUCT_DEVICE := lt03wifi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_MODEL=SM-P600 \
-    PRODUCT_NAME=lt03wifi \
-    PRODUCT_DEVICE=lt03wifi \
-    TARGET_DEVICE=lt03wifi
-    
-    # Device maintainer
-PRODUCT_BUILD_PROP_OVERRIDES := DEVICE_MAINTAINERS="Moshe Barash (mosimchah)"
+#Release name
+PRODUCT_RELEASE_NAME :=SAMSUNG SM-P600
+PRODUCT_MODEL :=lt03wifi
+PRODUCT_NAME :=orion_lt03wifi
+PRODUCT_MANUFACTURER :=samsung
 
 $(call inherit-product, device/samsung/lt03wifi/full_lt03wifi.mk)
+
+    # Device maintainer
+PRODUCT_BUILD_PROP_OVERRIDES := DEVICE_MAINTAINERS="Moshe Barash (mosimchah)"
