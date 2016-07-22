63bfe45aface01a8d06a4d0c322bdfde1c3f5ea1
mosimchah <mosimchah@gmail.com>
2016-07-22 18:17:34 -0400

    Update tesla.mk
    Now It will get past the "lunch" menu

--- tesla.mk
+++ tesla.mk
@@ -1,13 +1,11 @@
 # Inherit some common tesla stuff.
 $(call inherit-product, vendor/tesla/config/common_full_tablet_wifionly.mk)
 
-PRODUCT_NAME := tesla_lt03wifi
-PRODUCT_DEVICE := lt03wifi
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_MODEL=SM-P600 \
-    PRODUCT_NAME=lt03wifi \
-    PRODUCT_DEVICE=lt03wifi \
-    TARGET_DEVICE=lt03wifi
+#Release name
+PRODUCT_RELEASE_NAME :=SAMSUNG SM-P600
+PRODUCT_MODEL :=lt03wifi
+PRODUCT_NAME :=tesla_lt03wifi
+PRODUCT_MANUFACTURER :=samsung
 
 $(call inherit-product, device/samsung/lt03wifi/full_lt03wifi.mk)
+
